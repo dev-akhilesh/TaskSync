6837c752e75a375779c5109c895eab43f37a993a
Akhilesh Takawale <115489722+dev-akhilesh@users.noreply.github.com>
2024-03-28 05:22:05 +0530

    Delete Back-end directory

--- Back-end/Config/db.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose');
-require('dotenv').config();
-// const connection = mongoose.connect(`mongodb + srv://akhileshtakawale703:akhileshtakawale703@cluster0.47wrssa.mongodb.net/GreenMentor`);
-const connection = mongoose.connect(process.env.MONGO_URI);
-
-module.exports = connection;
